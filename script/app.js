366426ac5307fecf1fbeba222e51996e6113eb8a
Kristina <kzatesova@gmail.com>
2022-06-22 10:38:58 +0500

    theme

--- script/app.js
+++ script/app.js
@@ -185,3 +185,25 @@ function openTab(evt, tabName) {
   document.getElementById(tabName).style.display = "block";
   evt.currentTarget.className += " active";
 }
+
+
+
+
+
+// Theme 
+let changeThemeButtons = document.querySelectorAll('.changeTheme'); // Помещаем кнопки смены темы в переменную
+
+changeThemeButtons.forEach(button => {
+    button.addEventListener('click', function () { // К каждой добавляем обработчик событий на клик
+        let theme = this.dataset.theme; // Помещаем в переменную название темы из атрибута data-theme
+        applyTheme(theme); // Вызываем функцию, которая меняет тему и передаем в нее её название
+    });
+});
+
+function applyTheme(themeName) {
+    document.querySelector('[title="theme"]').setAttribute('href', `style/theme/theme-${themeName}.css`); // Помещаем путь к файлу темы в пустой link в head
+    changeThemeButtons.forEach(button => {
+        button.style.display = 'block'; // Показываем все кнопки смены темы
+    });
+    document.querySelector(`[data-theme="${themeName}"]`).style.display = 'none'; // Но скрываем кнопку для активной темы
+}
